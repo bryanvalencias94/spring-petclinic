0d16ddebb6dc60992796da8ebf5ddfe946fb518a
Bryan Valencia <bsuarez@atlassian.com>
2023-12-12 10:22:27 -0500

    Changing instance type

--- scripts/start_app.sh
+++ scripts/start_app.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 docker pull bryanvalencia94/petclinic:{CODEBUILD_RESOLVED_SOURCE_VERSION}
-docker run -p 8080:8080 bryanvalencia94/petclinic:{CODEBUILD_RESOLVED_SOURCE_VERSION}
+docker run -d -p 8080:8080 bryanvalencia94/petclinic:{CODEBUILD_RESOLVED_SOURCE_VERSION}
